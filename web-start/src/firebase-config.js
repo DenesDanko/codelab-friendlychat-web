adfb606a3182c8574dea1c494bf2c95bcf9e863d
Spartan <spartan@enigma.com>
2021-09-28 16:56:51 +0200

    Add config.js

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -1,14 +1,13 @@
+import config from 'config';
 /**
  * To find your Firebase config object:
- * 
+ *
  * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
  * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
-const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
-};
+
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
